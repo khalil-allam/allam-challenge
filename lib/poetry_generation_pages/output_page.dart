3b6dec93cb60f21fbca682b75ee6d26913104b67
Monder Bm <monder2322@gmail.com>
2024-11-02 17:58:19 +0300

    download output as pdf

--- lib/poetry_generation_pages/output_page.dart
+++ lib/poetry_generation_pages/output_page.dart
@@ -1,8 +1,15 @@
+import 'dart:io';
+
 import 'package:allam_challenge/coding_files/color_pallete.dart';
 import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 class OutputPage extends StatefulWidget {
   final String output_text;
@@ -83,19 +90,20 @@ class _OutputPageState extends State<OutputPage> {
               IconButton(
                 onPressed: () {
                   FlutterClipboard.copy(widget.output_text).then(
-                      (value) => ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text(
-                                "تم النسخ بنجاح",
-                                style: TextStyle(
-                                  color: whiteColor,
-                                  fontFamily: "Cairo",
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 15,
-                                ),
-                              ),
-                            ),
-                          ));
+                    (value) => ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text(
+                          "تم النسخ بنجاح",
+                          style: TextStyle(
+                            color: whiteColor,
+                            fontFamily: "Cairo",
+                            fontWeight: FontWeight.w600,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
                 },
                 icon: Container(
                   decoration: BoxDecoration(
@@ -109,20 +117,41 @@ class _OutputPageState extends State<OutputPage> {
                 ),
               ),
               IconButton(
-                onPressed: () {
-                  FlutterClipboard.copy(widget.output_text).then(
-                    (value) => ScaffoldMessenger.of(context)
-                        .showSnackBar(const SnackBar(
-                            content: Text(
-                      "تم النسخ بنجاح",
-                      style: TextStyle(
-                        color: whiteColor,
-                        fontFamily: "Cairo",
-                        fontWeight: FontWeight.w600,
-                        fontSize: 15,
+                onPressed: () async {
+                  final myTheme = pw.ThemeData.withFont(
+                    base: pw.Font.ttf(
+                      await rootBundle.load(
+                        "fonts/Cairo-Regular.ttf",
                       ),
-                    ))),
+                    ),
+                    bold: pw.Font.ttf(
+                      await rootBundle.load(
+                        "fonts/Cairo-Bold.ttf",
+                      ),
+                    ),
                   );
+                  final pdf = pw.Document(
+                    theme: myTheme,
+                  );
+
+                  pdf.addPage(
+                    pw.MultiPage(
+                      pageFormat: PdfPageFormat.a4,
+                      textDirection: pw.TextDirection.rtl,
+                      build: (pw.Context context) {
+                        return [
+                          pw.Paragraph(
+                            text: widget.output_text,
+                          ),
+                        ];
+                      },
+                    ),
+                  ); // Page
+                  var dir = await getApplicationDocumentsDirectory();
+                  final file = File(
+                      "${dir.path}/khalil_result_${DateTime.now().millisecondsSinceEpoch}.pdf");
+                  await file.writeAsBytes(await pdf.save());
+                  await OpenFile.open(file.path);
                 },
                 icon: Container(
                   decoration: BoxDecoration(
